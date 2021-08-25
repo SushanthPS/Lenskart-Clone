a3b1a56c769aac052e37fcc80c85d586b43bcc80
Sushanth <getsushanthps@gmail.com>
2021-08-25 23:54:17 +0530

    changed server file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const connect = require("./config/db");
+
+
 const productController = require("./controller/productController");
 const rowController = require("./controller/rowController");
+
+
 const app = express();
 app.use(express.json());
 let cors = require("cors");
@@ -11,7 +15,9 @@ app.use(cors());
 app.use("/product", productController);
 app.use("/row", rowController);
 
-app.listen(2345, async () => {
+
+const PORT = 2345;
+app.listen(PORT, async () => {
   await connect();
-  console.log("listening on port 2345");
+  console.log("listening on port " + PORT);
 });
