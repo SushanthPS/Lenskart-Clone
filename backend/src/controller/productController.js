936a41c7b6d264906a16fad986495f84e7e085d9
Sushanth <getsushanthps@gmail.com>
2021-08-29 18:11:39 +0530

    final after merge

---
+++ backend/src/controller/productController.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const Product = require("../model/productModel");
+const router = express.Router();
+
+router.post("", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    return res.send(product);
+  } catch (err) {
+    return res.send(err.message);
+  }
+});
+router.get("", async (req, res) => {
+  try {
+    const product = await Product.find().lean().exec();
+    let data = [];
+    for (let i = 0; i < product.length; i++) {
+      data.push(product[i]);
+    }
+    return res.send(data);
+  } catch (err) {
+    return res.send(err.message);
+  }
+});
+router.delete("/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    return res.send(product);
+  } catch (err) {
+    return res.send(err.message);
+  }
+});
+
+module.exports = router;
---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const connect = require("./config/db");
+
+const productController = require("./controller/productController");
+const rowController = require("./controller/rowController");
+const signupController = require("./controller/signupController");
+const cartController = require("./controller/cartController");
+
+const app = express();
+app.use(express.json());
+let cors = require("cors");
+app.use(cors());
+
+app.use("/product", productController);
+app.use("/row", rowController);
+app.use("/signup", signupController);
+app.use("/cart", cartController);
+
+const PORT = 2345;
+app.listen(PORT, async () => {
+  await connect();
+  console.log("listening on port " + PORT);
+});
