638d66a5aaa46b9f90cedbd0c64db9528e2bf9e5
Sushanth PS <35593841+sushanthps2@users.noreply.github.com>
2021-08-29 14:33:12 +0530

    latest working code v1

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,20 +1,27 @@
 let signUp = document.getElementById("sign-up");
 let signIn = document.getElementById("sign-in");
 var elements = document.getElementsByTagName("*");
+var body = document.getElementById("nav-body");
 
 function signup() {
     signUp.style.display = "flex";
     signIn.style.display = "none";
+    body.style.opacity = "0.9";
+    body.style.filter = "brightness(50%)"
 }
 
 function signin() {
     signUp.style.display = "none";
     signIn.style.display = "flex";
+    body.style.opacity = "0.9";
+    body.style.filter = "brightness(50%)"
 }
 
 function closeOption() {
     signUp.style.display = "none";
     signIn.style.display = "none";
+    body.style.opacity = "1";
+    body.style.filter = "brightness(100%)"
 }
 
 async function register(e) {
@@ -48,33 +55,26 @@ async function register(e) {
 
 
 
-
 async function login(e) {
     e.preventDefault();
-    // let passInput = document.getElementById("signin-pass");
-    // if (passInput.style.display == "block") {
-    //     if (passInput.value == user.password) {
-    //         alert("Login Successful");
-    //         window.location.href = "homepage.html"
-    //     } else {
-    //         alert("Either username or password is incorrect.")
-    //     }
-    // }
+
     let input = document.getElementById("mobile-email").value;
-    if (input == "")
-        alert("Please enter a valid Email or Mobile Number.");
+    let passInput = document.getElementById("signin-pass").value;
+
+    if (input == "" || passInput == "")
+        alert("Please fill in all the fields");
     else {
+        let user;
         let res = await fetch("http://localhost:2345/signup");
         let data = await res.json();
-        let user;
         data.forEach((el) => {
             if (input == el.email || input == el.mobile) {
-                user = el;
-                // passInput.style.display = "block";
+                if (passInput == el.password)
+                    user = el;
             }
         })
-        if (user == undefined)
-            alert("Account doesn't exist. Use different phone number or email.");
+        if (user == undefined || user == null)
+            alert("Please enter a valid Email or Mobile Number and a Password");
         else {
             localStorage.setItem("loggedUser", JSON.stringify(user));
             alert("Login Successful")
